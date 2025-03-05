b9f91b50d76fb0a93799fef2057bd4e5c4672857
zhihui.lin <zhihui.lin@avatr.com>
2025-03-05 18:05:49 +0800

    add

---
+++ 45.cpp
@@ -0,0 +1,23 @@
+//45. 跳跃游戏 II
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> v1 = {3,2,1,0,4};
+    int res = s.jump(v1);
+    cout << res << "\n";
+    for (auto i : v1) {
+        cout << i << endl;
+    }
+    return 0;
+}
--- 55.cpp
+++ 55.cpp
@@ -10,14 +10,18 @@ public:
     bool canJump(vector<int>& nums) {
         int maxReach = 0;
         for (int i=0; i<nums.size(); ++i) {
-            
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = max(maxReach, i + nums[i]);
         }
+        return maxReach >= nums.size()-1;
     }
 };
 
 int main() {
     Solution s;
-    vector<int> v1 = {1,2,3,4,5};
+    vector<int> v1 = {3,2,1,0,4};
     int res = s.canJump(v1);
     cout << res << "\n";
     for (auto i : v1) {
